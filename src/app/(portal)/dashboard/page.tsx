333f5c68777bf22927f350c8e5fc83d4137e201d
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-04-29 16:21:03 +0300

    feat: dashboard for employee portal setup

--- src/app/(portal)/dashboard/page.tsx
+++ src/app/(portal)/dashboard/page.tsx
@@ -1,13 +1,32 @@
 import { Metadata } from 'next';
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import DashboardCards from './_components/dashboard-cards';
+import LeaveApplications from './_components/leave-applications';
+
 export const metadata: Metadata = {
   title: 'Dashboard',
 };
 
 export default function DashboardPage() {
   return (
-    <div>
-      <p>DashboardPage</p>
+    <div className="space-y-8">
+      <DashboardCards />
+      <Card>
+        <CardHeader>
+          <CardTitle>Leaves applied.</CardTitle>
+          <CardDescription>List of all days applied.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <LeaveApplications />
+        </CardContent>
+      </Card>
     </div>
   );
 }
